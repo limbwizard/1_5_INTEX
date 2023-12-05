68bb37f30dc781ed4f764244f674ec7f1b345926
sydneygroverr <sydney.grover8@gmail.com>
2023-12-05 10:33:04 -0700

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const knex = require("knex")({
     user: process.env.RDS_USERNAME || "intex",
     password: process.env.RDS_PASSWORD || "intexroot15",
     database: process.env.RDS_DB_NAME || "MentalHealthSM",
-    port: process.env.RDS_PORT || 5432
+    port: process.env.RDS_PORT || 5432,
+    ssl: process.env.DB_SSL ? {rejectUnauthorized: false} : false
     }
 });
