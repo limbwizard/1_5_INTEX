d62b7d407199a47cbd71fb052716370bf9f773df
sydneygroverr <sydney.grover8@gmail.com>
2023-12-05 11:15:22 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-// let express = require("express");
-
-// let app = express();
-
-// app.get("/", (req, res) => res.send("this is the server response to the client request"));
-
-// app.listen(3000, () => console.log("the server is listening for a client"));
 
 const express = require("express");
 let app = express();
